cc7355403447a4e69ef6a9b8fbd183447d11da2c
jhoff-dev <dev.jhoff@gmail.com>
2025-08-11 20:58:25 -0300

    HRS-20 Add commands makefile

---
+++ commands/build.mk
@@ -0,0 +1,41 @@
+# Start the Docker services
+up:
+	${DOCKER_COMPOSE} up -d
+
+# Stop the Docker services
+stop:
+	${DOCKER_COMPOSE} stop
+
+# Destroy the Docker services
+destroy:
+	${DOCKER_COMPOSE} down --volumes
+
+# Restart the Docker services
+restart:
+	${DOCKER_COMPOSE} restart
+
+# Run Composer commands directly in the theme folder
+composer:
+	${DOCKER_COMPOSE} exec -i -t composer sh -c 'composer $(filter-out $@,$(MAKECMDGOALS))'
+
+# Run Yarn commands directly in the theme folder
+yarn:
+	${DOCKER_COMPOSE} exec -u node -i -t node sh -c 'corepack yarn $(filter-out $@,$(MAKECMDGOALS))'
+
+# Run WP-CLI commands directly in the theme folder
+wp:
+	${DOCKER_COMPOSE} exec -i -t wp-cli sh -c 'wp $(filter-out $@,$(MAKECMDGOALS))'
+
+# Fix file and directory permissions
+permissions:
+	sudo chown -R ${UID}:${GID} *; \
+	sudo chmod -R 777 *; \
+	git config core.fileMode false; \
+
+# Clear caches and optimize the application
+clear:
+	${DOCKER_COMPOSE} exec -i -t wp-cli sh -c 'wp cache flush'
+	${DOCKER_COMPOSE} exec -i -t wp-cli sh -c 'wp transient delete --all'
+	${DOCKER_COMPOSE} exec -i -t composer sh -c 'composer dump -o'
+	${DOCKER_COMPOSE} exec -i -t wp-cli sh -c 'wp acorn package:discover'
+	${DOCKER_COMPOSE} exec -i -t wp-cli sh -c 'wp acorn optimize:clear'
---
+++ commands/ssl.mk
@@ -0,0 +1,12 @@
+SSL_CERT_NAME=localhost
+SSL_CERT_KEY=$(SSL_CERT_NAME).key
+SSL_CERT_CSR=$(SSL_CERT_NAME).csr
+SSL_CERT_CRT=$(SSL_CERT_NAME).crt
+SSL_CERT_DAYS=3650 # 10 years
+SSL_CERT_SUBJECT=/C=BR/ST=RS/L=RG/O=JHOFF/CN=$(SSL_CERT_NAME)
+
+create-ssl:
+	openssl genrsa -out $(SSL_CERT_KEY) 2048
+	openssl req -new -key $(SSL_CERT_KEY) -out $(SSL_CERT_CSR) -subj "$(SSL_CERT_SUBJECT)"
+	openssl x509 -req -days $(SSL_CERT_DAYS) -in $(SSL_CERT_CSR) -signkey $(SSL_CERT_KEY) -out $(SSL_CERT_CRT)
+	rm -f $(SSL_CERT_CSR)
