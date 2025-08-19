de10ebc4ca767fd5b3fa539bb13703548bca8c1b
jhoff-dev <dev.jhoff@gmail.com>
2025-08-19 19:05:06 -0300

    HRS-30 Configure the ViteJS server

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+    },
+  },
   base: '/app/themes/half-horizons/public/build/',
   plugins: [
     tailwindcss(),
