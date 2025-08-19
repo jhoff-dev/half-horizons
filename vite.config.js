fc3e6bbbab8b923069579d91eda11b1700bc0dcd
jhoff-dev <dev.jhoff@gmail.com>
2025-08-19 15:53:10 -0300

    HRS-30 Adjust the information and add the libraries that will be used in the project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite';
-import laravel from 'laravel-vite-plugin'
 import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/app/themes/sage/public/build/',
+  base: '/app/themes/half-horizons/public/build/',
   plugins: [
     tailwindcss(),
     laravel({
