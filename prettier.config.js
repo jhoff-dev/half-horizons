f5f8dff4f0f2ec33b613ad27d112595938aee6be
jhoff-dev <dev.jhoff@gmail.com>
2025-08-23 19:47:54 -0300

    HRS-30 Adjust Prettier options

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,36 @@
 // prettier.config.js
 export default {
-  "plugins": ["prettier-plugin-tailwindcss", "prettier-plugin-blade"],
-  "printWidth": 120,
-  "tabWidth": 2,
-  "useTabs": true,
-  "semi": true,
+  plugins: ["@shufo/prettier-plugin-blade", "prettier-plugin-tailwindcss"],
+  // Core
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "ignore",
+  endOfLine: "lf",
+  singleAttributePerLine: true,
+  embeddedLanguageFormatting: "auto",
+  // Blade
+  wrapAttributes: "aligned-multiple",
+  wrapAttributesMinAttrs: 2,
+  sortTailwindcssClasses: false,
+  sortHtmlAttributes: "alphabetical",
+  noPhpSyntaxCheck: false,
+  indentInnerHtml: true,
+  extraLiners: "",
+  trailingCommaPHP: true,
+  componentPrefix: "x-,livewire:",
+  phpVersion: "8.2",
+
+  // Tailwind
+  tailwindPreserveWhitespace: false,
+  tailwindPreserveDuplicates: false,
 };
